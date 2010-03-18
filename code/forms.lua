84493c0b015f720e8d478afa0fb8c00f08e6290e
George Boyle <george@thebuds.net>
2010-03-18 00:42:03 +0000

    Implemented if and let

--- code/forms.lua
+++ code/forms.lua
@@ -4,6 +4,25 @@ require("parser")
 require("scanner")
 
 --
+-- A utility function that returns the string representation of a sequence of
+-- scheme expressions (or "body"s) from the scanner, returning the last one.
+-- This is a common syntax for lambda, case-lambda, cond, let and others
+--
+local function bodyList()
+    local result = ""
+    local token = scanner.peekToken()
+    while token ~= ")" do
+        local expr = parser.parse(scanner.nextToken())
+        if scanner.peekToken() == ")" then
+            result = result .. "return "
+        end
+        result = result .. expr:selfAsString() .. "\n"
+        token = scanner.peekToken()
+    end
+    return result
+end;
+
+--
 -- A table of supported special syntaxes. Each is a function that returns the
 -- final output string for that syntax.
 --
@@ -25,25 +44,22 @@ specialSyntax = {
                     result = result .. "elseif "
                     .. test:selfAsString() .. ".value then\n"
                 end
-                local expr = parser.parse(scanner.nextToken())
-                if tostring(expr) == "=>" then
+                local expr = scanner.peekToken()
+                if expr == "=>" then
+                    scanner.nextToken()
                     local func = parser.parse(scanner.nextToken())
                     result = result .. "return " .. func:selfAsString() ..
                     "(" .. test:selfAsString() .. ")\n"
-                elseif tostring(expr) == ")" then
+                elseif expr == ")" then
+                    scanner.nextToken()
                     result = result .. "return " .. test:selfAsString() .. "\n"
                 else
-                    while tostring(expr) ~= ")" do
-                        if scanner.peekToken() == ")" then
-                            result = result .. "return "
-                        end
-                        result = result .. expr:selfAsString() .. "\n"
-                        expr = parser.parse(scanner.nextToken())
-                    end
+                    result = result .. bodyList()
+                    scanner.nextToken()
                 end
                 token = scanner.peekToken()
             end
-            return result .. "end\nend)()"
+            return result .. "end end)()\n"
         end;
 
     ["define"] = function ()
@@ -51,6 +67,18 @@ specialSyntax = {
         local assignee = parser.parse(scanner.nextToken())
         local object = parser.parse(scanner.nextToken())
         return assignee:selfAsString() .. " = " .. object:selfAsString()
+    end;
+
+    ["if"] = function ()
+        local result = "(function ()\nif "
+            .. parser.parse(scanner.nextToken()):selfAsString()
+            .. ".value then\nreturn"
+            .. parser.parse(scanner.nextToken()):selfAsString() .. "\n"
+        if scanner.peekToken() ~= ")" then
+            result = result .. "else\nreturn"
+            .. parser.parse(scanner.nextToken()):selfAsString() .. "\n"
+        end
+        return result .. "end end)()\n"
     end;
 
     ["lambda"] = function ()
@@ -76,9 +104,37 @@ specialSyntax = {
                 result = result .. "local " .. tostring(vararg)
                 .. " = scmList.fromTable{...}\n"
             end
-            result = result .. "return "
-            .. tostring(parser.parse(scanner.nextToken())) .. "\nend)"
+            result = result .. bodyList() .. "end)"
         return result
+    end;
+
+    ["let"] = function ()
+        local funcName = nil
+        if scanner.peekToken() == "Symbol" then
+            funcName = tostring(parser.parse(scanner.nextToken()))
+        end
+        scanner.nextToken() -- Opening '(' of var list
+        local varNames = {}
+        local varValues = {}
+        local token = scanner.peekToken()
+        while token ~= ")" do
+            scanner.nextToken() -- Opening '(' of var definition
+            table.insert(varNames, parser.parse(scanner.nextToken()))
+            table.insert(varValues, parser.parse(scanner.nextToken()))
+            scanner.nextToken() -- Closing ')' of var definition
+            token = scanner.peekToken()
+        end
+        scanner.nextToken() -- Closing ')' of var list
+        local funcDef = "(function "
+            .. "(" .. scmArglist.fromTable(varNames):selfAsString() .. ")\n"
+            .. bodyList() .. "end)"
+        local args = scmArglist.fromTable(varValues):selfAsString()
+        if funcName then
+            return "(function ()\n" .. funcName .. " = " .. funcDef
+                .. "\nreturn " .. funcName .. "(" .. args .. ") end)()\n"
+        else
+            return funcDef .. "(" .. args .. ")\n"
+        end
     end;
 }
 
