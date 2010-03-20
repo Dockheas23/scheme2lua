48ee1dafccb9ce7874fa3cecb8b65797383418ff
George Boyle <george@thebuds.net>
2010-03-20 12:08:57 +0000

    Minor changes to primes.scm

--- code/primes.scm
+++ code/primes.scm
@@ -3,12 +3,12 @@
     (let nextprime ((primecount 1) (current 2))
       (cond
         ((> primecount n) (newline))
-        ((isprime? current)
-         (display current) (display " ")
+        ((isPrime current)
+         (display primecount) (display ": ") (display current) (newline)
          (nextprime (+ primecount 1) (+ current 1)))
         (else (nextprime primecount (+ current 1)))))))
 
-(define isprime?
+(define isPrime
   (lambda (n)
     (if (< n 2) #f
       (let prime ((k 2))
@@ -17,5 +17,5 @@
           ((integer? (/ n k)) #f)
           (else (prime (+ k 1))))))))
 
-(primes 1000)
+(primes 10000)
 (exit)
